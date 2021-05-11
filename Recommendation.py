0ccf127220d52c229cfad48f94bf03cf370e06f6
npansuriya17 <73963997+npansuriya17@users.noreply.github.com>
2021-05-11 16:43:51 -0500

    Delete Recommendation.py

--- Recommendation.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-rcols = ['user_id','movie_id','rating']
-ratings = pd.read_csv(r'C:\Users\win\Downloads\udemy-frank-master\udemy-frank-master\ml-100k\u.data', sep='\t', names= rcols, usecols = range(3))
